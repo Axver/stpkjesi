f7dd4a3f27bf3da5d1495bca41f8cec282c4eedb
axver <ecimoramora@gmail.com>
2018-04-21 16:26:25 +0700

    Fungsi Potong-potong array dibuat

--- proses/ambildata.php
+++ proses/ambildata.php
@@ -32,7 +32,51 @@ $kolom= count($hasil[0]);
 // echo $hasil[0][1];
 // echo $hasil[1][0];
 
-// Mendapatkan data max untuk tiap-tiap kolom
+// Mendapatkan data tiap-tiap kolom
+
+$w=0;
+$kolom1=$kolom-1;
+$simpan=[];
+
+while($w<$kolom1)
+{
+  for($x=0;$x<$kolom1;$x++)
+  {
+     $simpan[$w][$x]=$hasil[$x][$w];
+  }
+  // $simpan[$w]=$
+
+  $w++;
+}
+
+//Olah Array Multidimensi masing-masing Kolom
+$maksimum=[];
+// echo max($simpan[0]);
+// echo count($simpan);
+$jumbaris=count($simpan);
+$y=0;
+while($y<$jumbaris)
+   {
+     $maksimum[$y]=max($simpan[$y]);
+     echo $maksimum[$y];
+     $y++;
+   }
+
+$minimun=[];
+
+  $jumbaris=count($simpan);
+  $z=0;
+   while($z<$jumbaris)
+      {
+        $minimum[$z]=min($simpan[$z]);
+        echo $minimum[$z];
+        $z++;
+      }
+
+
+//Olah data masing-masing dengan rumus yg ada
+
+
 
 
  ?>
